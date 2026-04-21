09996a52e1609bc842f8960a9c8cc4e48a7a5b19
irsanwoy <irsanwoy54@gmail.com>
2026-04-22 00:32:42 +0700

    chore: update to new functional supabase project

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,10 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("--- SUPABASE DEBUG START ---");
-console.log("URL From Env:", supabaseUrl);
-console.log("Valid URL Format?", supabaseUrl?.startsWith('https://'));
-console.log("Is haphv present?", supabaseUrl?.includes('haphv'));
-console.log("--- SUPABASE DEBUG END ---");
-
 export const supabase = createClient(supabaseUrl, supabaseKey);
